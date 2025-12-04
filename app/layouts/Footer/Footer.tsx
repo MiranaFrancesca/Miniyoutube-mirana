c2b1599f83f904123833c5b89aa87d517a08cc31
Mirana <miranafrancesca.projet@gmail.com>
2025-12-04 16:11:56 +0100

    Initial commit

--- app/layouts/Footer/Footer.tsx
+++ app/layouts/Footer/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
         <footer className="p-4 text-sm text-zinc-500">
-            MiniTube © 2025 — Projet React Mirana
+            MiniTube © 2025 — Projet React Groupe
         </footer>
     );
 }
